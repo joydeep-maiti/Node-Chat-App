28253ed7fe22e6707e691c80adae2a5b23cd770c
Joydeep Maiti <joydeep127@gmail.com>
2018-02-25 15:13:59 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+var app = express();
+app.use(express.static(publicPath));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> {
+    console.log(`Server started on ${port}`);
+})
